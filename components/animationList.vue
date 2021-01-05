9c50f5ee0fb1435b43b6ba8674068975ca458793
Your Name <you@example.com>
2021-01-05 18:11:20 +0800

    修改导航栏的实现方式

---
+++ components/animationList.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class="animation-list" :style="isShowStyle">
+		<ul>
+			<li v-for="(item, index) in list" :key="index" @tap="optionsClick(index)">{{item}}</li>
+		</ul>
+	</view>
+</template>
+
+<script>
+	export default {
+		components: {
+
+		},
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			
+		},
+		props: {
+			list: {
+				type: Array,
+				default(){
+					return []
+				}
+			},
+			opacity: {
+				type: Number,
+				default: 0
+			}
+		},
+		computed: {
+			isShowStyle() {
+				let _this = this;
+				return {
+					opacity: _this.opacity
+				}
+			}
+		},
+		methods: {
+			optionsClick(index){
+				this.$emit('optionsClick', index)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.animation-list {
+		position: absolute;
+		top: 44px;
+		right: 0;
+		padding: 0 20rpx;
+		transition: all .5s;
+		background-color: #fff;
+		ul {
+			li {
+				padding: 20rpx 20rpx;
+				border-bottom: 1px solid #eee;
+			}
+			li:last-child {
+				border-bottom: 0;
+			}
+		}
+	}
+</style>
